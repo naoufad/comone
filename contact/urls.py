2fbad3eaa6dfb80b981dc6da46a629add1e994cd
naoufad <naoufad@icloud.com>
2018-02-05 14:38:33 +0100

    contact & footer

--- contact/urls.py
+++ contact/urls.py
@@ -1,9 +1,19 @@
 from django.conf.urls import url
-from contact.views import ContactView
+from contact.views import ContactView, Partenaires, MentionsLegales,Nous,Batiment,Allimentation,Riz,cg,Services
 from . import views 
 app_name='contact'
 
 urlpatterns = [
     url(r'^$',ContactView.as_view(), name='contact'),
     url(r'^facebook/',views.facebookView, name='facebook'),
+    url(r'^partenaires/',Partenaires.as_view(),name='partenaires'),
+    url(r'^legales/',MentionsLegales.as_view(),name='legales'),
+    url(r'^cg/',cg.as_view(),name='cg'),
+    url(r'^services/',Services.as_view(),name='services'),
+    url(r'^nous/',Nous.as_view(),name='nous'),
+    url(r'^batiment/',Batiment.as_view(),name='batiment'),
+    url(r'^allimentation/',Allimentation.as_view(),name='alliment'),
+    url(r'^riz/',Riz.as_view(),name='riz'),
+    
+    
 ]
--- contact/views.py
+++ contact/views.py
@@ -10,4 +10,37 @@ class ContactView(TemplateView):
     template_name='yous/contact/contact_home.html'
 
 
+class Partenaires(TemplateView):
+    template_name='yous/contact/partenaires.html'
 
+class Services(TemplateView):
+    template_name='yous/contact/services.html'
+
+
+class MentionsLegales(TemplateView):
+    template_name='yous/contact/mentions_legales.html'
+
+
+class cg(TemplateView):
+    template_name='yous/contact/conditions_generales.html'
+
+
+class Nous(TemplateView):
+    template_name='yous/contact/nous.html'
+
+
+class Batiment(TemplateView):
+    template_name='yous/contact/batiment.html'
+
+
+class Allimentation(TemplateView):
+    template_name='yous/contact/allimentation.html'
+
+
+
+class Riz(TemplateView):
+    template_name='yous/contact/riz.html'
+
+
+
+    
